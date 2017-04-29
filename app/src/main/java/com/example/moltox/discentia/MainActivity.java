e4953a42878bb8cbcb1274a8c2a24e1cb26fb6da
moltox <masterm_mh@hotmail.de>
2017-04-29 13:04:14 +0200

    Moved Buttons to NavDrawer

--- app/src/main/java/com/example/moltox/discentia/MainActivity.java
+++ app/src/main/java/com/example/moltox/discentia/MainActivity.java
@@ -22,13 +22,11 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = MainActivity.class.getName();
-    View view;
     int count;
-    Button m_btn_takeCard;
     TextView m_tv_main;
     Button btn_debugActivity;
-    Button btn_download;
     Button btn_showCards;
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,49 +47,20 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         m_tv_main = (TextView) findViewById(R.id.id_tv_main);
-        m_btn_takeCard = (Button) findViewById(R.id.btn_takeCard);
-        m_btn_takeCard.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                callQandaActivity(v);
-            }
-        });
-        btn_showCards = (Button) findViewById(R.id.btn_showCards);
-        btn_showCards.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                callShowCardsActivity(v);
-            }
-        });
-
-        btn_debugActivity = (Button) findViewById(R.id.btn_debugActivity);
-        btn_debugActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                callDebugActivy(v);
-            }
-        });
-        btn_download = (Button) findViewById(R.id.btn_download);
-        btn_download.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                callDownloadActivity(v);
-            }
-        });
-
         Log.v(TAG, "Serialnr: " + Build.SERIAL
                 + "\nFingerprint: " + Build.FINGERPRINT
                 + "\nBoard: " + Build.BOARD
                 + "\nBrand: " + Build.BRAND
                 + "\nBoatloader: " + Build.BOOTLOADER
                 + "\nDisplay: " + Build.DISPLAY
-                + "\nHardware: " +Build.HARDWARE
+                + "\nHardware: " + Build.HARDWARE
                 + "\nHost: " + Build.HOST
                 + "\nID: " + Build.ID
                 + "\nManufacturer: " + Build.MANUFACTURER
@@ -141,18 +110,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_callQanda) {
+            callQandaActivity(view);
+        } else if (id == R.id.nav_call_downloadCards) {
+            callDownloadActivity(view);
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        }  else if (id == R.id.nav_showAllCards) {
+            callShowCardsActivity(view);
+        } else if (id == R.id.nav_debugActivity) {
+            callDebugActivy(view);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
